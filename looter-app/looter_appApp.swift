dca95b5187138ed4134b548448800523fd931bd6
thomaspsl <thomas.peissel.tp@gmail.com>
2024-10-08 18:08:25 +0200

    Initial Commit

---
+++ looter-app/looter_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  looter_appApp.swift
+//  looter-app
+//
+//  Created by Thomas on 08/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct looter_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
